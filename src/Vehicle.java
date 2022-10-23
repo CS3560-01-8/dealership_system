4929ffb89d47376c6f8ea398ece7b429f9a9867d
Floomf <mrfrozenfirefly@gmail.com>
2022-10-22 22:10:28 -0700

    Add constructor and methods to Vehicle

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -11,4 +11,24 @@ class Vehicle {
     private int listingPrice;
     private String status;
 
+    Vehicle(String vin, String make, String model, int year, String condition, int mileage, int listingPrice, String status) {
+        this.vin = vin;
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.condition = condition;
+        this.mileage = mileage;
+        this.listingPrice = listingPrice;
+        this.status = status;
+    }
+
+    //will return element for displaying in gui
+    void getDisplayElement() {
+
+    }
+
+    void sell() {
+        status = "sold";
+    }
+
 }
