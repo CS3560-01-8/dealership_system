6aec933e811ddf3f43b97447744b6d320188c342
Austin Aluzzi <aaaluzzi@cpp.edu>
2022-11-13 23:29:46 -0800

    Revert "Remove main methods from JFrame classes"
    This reverts commit 8b32e62542b27d8e785b33c5a4af98f72100b244.

--- src/main/java/dealership/Main.java
+++ src/main/java/dealership/Main.java
@@ -1,8 +1,6 @@
 package dealership;
 
 import dealership.db.DatabaseConnector;
-import dealership.ui.CreateAccount;
-import dealership.ui.MenuScreen;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,8 +12,6 @@ public class Main {
             System.out.println("Please provide your MYSQL password as the first command line argument.");
             System.exit(1);
         }
-        createGui();
-
         DatabaseConnector.createConnection(args[0]);
 
         //Example query
@@ -33,33 +29,4 @@ public class Main {
         }
     }
 
-    public static void createGui() {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CreateAccount.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CreateAccount.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CreateAccount.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CreateAccount.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MenuScreen().setVisible(true);
-            }
-        });
-    }
 }
