62f6e3b7174c9ecb42c2ad9f2e71c794ada944ad
Programmer310 <89231213+Programmer310@users.noreply.github.com>
2022-11-28 09:27:13 -0800

    Added comments to describe what each method did in AccountHandler

--- src/main/java/dealership/controller/AccountHandler.java
+++ src/main/java/dealership/controller/AccountHandler.java
@@ -7,8 +7,10 @@ import dealership.object.Employee;
 
 public class AccountHandler {
 
+    // logged in account
     private static Account loggedInAccount;
 
+    // create and save customer account to database
     public static boolean createCustomerAccount(String email, String password, String firstName, String lastName, String phoneNum, String address) {
         if (Customer.exists(email)) {
             return false;
@@ -17,22 +19,27 @@ public class AccountHandler {
         AccountDB.writeCustomer(new Customer(email, firstName, lastName, phoneNum, address), password);
         return true;
     }
-
+    
+    // test if login is valid; true if so, false otherwise
     public static boolean tryLogin(String email, String password) {
         loggedInAccount = Account.getAccount(email, password);
         return loggedInAccount != null;
     }
 
+    // test if user is logged into an account; true if so, false otherwise
     public static boolean isLoggedIn() {
         return loggedInAccount != null;
     }
 
+    // get name of logged in user
     public static String getLoggedInName() {
         return loggedInAccount.getName();
     }
 
+    // get email of logged in user
     public static String getLoggedInEmail() { return loggedInAccount.getEmail(); }
 
+    // test if account is for an employee
     public static boolean isEmployee()
     {
         return (loggedInAccount instanceof Employee);
