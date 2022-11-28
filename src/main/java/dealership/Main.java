0e9f87975f20f3eb7aa53a3751259c82329d3e89
Austin Aluzzi <aaaluzzi@cpp.edu>
2022-11-27 22:30:06 -0800

    Add FlatLaf dependency and set look and feel to it

--- src/main/java/dealership/Main.java
+++ src/main/java/dealership/Main.java
@@ -1,67 +1,20 @@
 package dealership;
 
-import dealership.access.AccountDB;
-import dealership.access.AppointmentDB;
-import dealership.access.SaleDB;
-import dealership.controller.AccountHandler;
-import dealership.controller.AppointmentHandler;
+import com.formdev.flatlaf.FlatLightLaf;
 import dealership.db.DatabaseConnector;
-import dealership.object.Customer;
-import dealership.object.Sale;
 import dealership.ui.MainScreen;
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
+import javax.swing.*;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedLookAndFeelException {
         if (args.length == 0) {
             System.out.println("Please provide your MYSQL password as the first command line argument.");
             System.exit(1);
         }
+
         DatabaseConnector.createConnection(args[0]);
-
-        //Example query
-        /*ResultSet rs = DatabaseConnector.executeQuery("SELECT * FROM sale");
-        System.out.println("Sales Table:");
-        try {
-            while (rs.next()) {
-                for (int i = 1; i < rs.getMetaData().getColumnCount(); i++) {
-                    System.out.print(rs.getString(i) + " ");
-                }
-                System.out.println();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }*/
-
-        //test appointment
-        //AppointmentHandler.makeAppointment("bringe4@naver.com", "1C6RD7GP5CS629292", "2022-11-18 01:36:00");
-        //System.out.println(AppointmentDB.checkAvailability("hzoellner0@nhs.uk", "WAUDF58E85A177360", "2002-09-04 12:00:00"));
-        //AppointmentDB.getAppointments("hzoellner0@nhs.uk");
-
-        //test getEmployeeEmail
-        //System.out.println(AccountDB.getEmployeeEmails().toString());
-
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-
+        UIManager.setLookAndFeel(new FlatLightLaf());
         new MainScreen().setVisible(true);
     }
 
